0552915fb730e6d7a299eb244c0728bf9a89ae2c
Eduardo Correia <93390976+eduardo-mdc@users.noreply.github.com>
2023-03-25 16:32:38 +0000

    added initial algorithm structure

--- src/states/solver.py
+++ src/states/solver.py
@@ -3,15 +3,38 @@ from pygame.locals import *
 from states.gamestate import *
 from states.player import *
 from main import * 
-import random
+from copy import deepcopy
+from collections import deque
+from treenode import *
 
-class Solver():
+class Solver:
     def __init__(self,maze,player):
         self._maze = maze
         self._player = player
 
     def solve(self):
         moves = self._player.getMoves()
-        print("SOLVING")
-    
+        self.solve_BFS()
 
+    def solve_BFS(self):
+        print("Solving with BFS")
+        root = TreeNode(self._player._block_state)   # create the root node in the search tree
+        queue = deque([root])   # initialize the queue to store the nodes
+
+        while queue:
+            node = queue.popleft()   # get first element in the queue
+            node.check_depth()
+            if node.state.check_goal(self._maze):   # check goal state
+                return node
+            
+            for state in node.state.child_block_states(self._maze):   # go through next states
+                # create tree node with the new state
+                leaf = TreeNode(state[1])
+                
+                # link child node to its parent in the tree
+                node.add_child(leaf)
+                
+                # enqueue the child node
+                queue.append(leaf)
+        return None
+    
---
+++ src/treenode.py
@@ -0,0 +1,28 @@
+# A generic definition of a tree node holding a state of the problem
+class TreeNode:
+    def __init__(self, state, parent=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.check_depth()
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.state == other.state
+        else:
+            return False
+
+    def add_child(self, child_node):
+        self.children.append(child_node)
+        child_node.parent = self
+        self.check_depth()
+    
+    def check_depth(self):
+        if self.parent is None:
+            self.depth = 0
+        else:
+            self.depth = self.parent.depth + 1
+
+    def __str__(self):
+        return str(self.state)
+    
